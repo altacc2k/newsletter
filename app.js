2cb22e834ce17a72a3a51f71f9dbdaaa4edc460f
altacc2k <steamsidb@gmail.com>
2020-12-08 06:48:35 +0530

    3rd try

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const https = require("https")
-heroku config:set NODE_ENV=production
 
 const app = express()
 app.use(bodyParser.urlencoded({
