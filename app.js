32b52fa68e6c60cc793a41ff7cea0ea2ac95c978
altacc2k <steamsidb@gmail.com>
2020-12-08 06:52:09 +0530

    final

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("Public"));
+app.use(express.static("public"));
 
 
 app.get("/", function(req, res) {
